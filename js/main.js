3eb34c04b24263b6a0ac4bc3c483ccdb24e3fbf7
Kribas Rimal <carlousrimal@gmail.com>
2022-01-20 18:02:28 +0545

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 50) {
+        $(".header").addClass("header-scrolled");
+    } else {
+        
+       $(".header").removeClass("header-scrolled");
+    }
+});
